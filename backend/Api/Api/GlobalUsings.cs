c9159ecd2f3b3deb14f08c2514d810aecf8cf5ca
Nikola Podrascanin <nikola.podrascanin@eng.it>
2026-05-20 08:24:53 +0200

    commit first changes

---
+++ backend/Api/Api/GlobalUsings.cs
@@ -0,0 +1,20 @@
+﻿global using System.Reflection;
+global using System.Text;
+global using Carter;
+global using FluentValidation;
+global using Newtonsoft.Json;
+global using MediatR;
+global using Mapster;
+global using Microsoft.EntityFrameworkCore;
+global using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+
+global using Api.Shared.ExcelFileExporting;
+global using Api.Shared.ExcelFileExporting;
+global using Api.Behaviors;
+global using Api.Extensions;
+global using Api.Abstractions.CQRS;
+global using Api.Abstractions.ErrorResponse;
+global using Api.Abstractions.ResultResponse;
+global using Api.Database.Entities;
+global using Api.Database;
